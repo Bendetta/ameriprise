4922f043194fd82702a5c21b73d32f1d4b2ff978
Brad Anderson <banderso@nerdery.com>
2015-10-19 14:29:17 -0500

    Adds jquery and switches out node connect for express.

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
+var express = require('express');
+var bodyParser = require('body-parser')
 
 
 var PUBLIC_DIR = 'public';
 var port = process.env.PORT || 3000;
-var app = connect();
+var app = express();
 
-app.use(serveStatic(PUBLIC_DIR));
+// Static pages
+app.use(express.static('public'));
+
+// Set up request body parser
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
+
+// Routes
+app.post('/test', function (req, res) {
+    console.log(req.body);
+    res.send();
+});
+
 app.listen(port);
 
 console.log('Listening on ' + port);
